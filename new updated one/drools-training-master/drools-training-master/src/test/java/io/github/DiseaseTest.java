d30c6245ced11d4bded723b6b7a0fa0afa430760
Ditipriya Ganguly <77203994+Ditipriya04Ganguly@users.noreply.github.com>
2022-07-13 14:20:48 +0530

    Add files via upload

---
+++ new updated one/drools-training-master/drools-training-master/src/test/java/io/github/DiseaseTest.java
@@ -0,0 +1,47 @@
+package io.github;
+
+import model.Patient;
+import model.PatientDisease;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieSession;
+
+import java.time.LocalDate;
+
+public class DiseaseTest {
+
+
+    /*@Test
+    public void testForIcd()
+    {
+        KieSession ksession=KieServices.Factory.get().getKieClasspathContainer().newKieSession("diseaserules");
+        Patient p= new Patient(1,"R13.1", "Active", LocalDate.of(2022,06,30) );
+        PatientDisease patientDisease= new PatientDisease();
+        ksession.insert(p);
+        ksession.insert(patientDisease);
+        ksession.fireAllRules();
+        ksession.dispose();
+        assert(patientDisease.getMaplist().get(p).contains("Dysphagia"));
+    }
+
+    @Test
+    public void testForGerd()
+    {
+        Patient p= new Patient(2,"K21.0", "Active", LocalDate.of(2022,06,30) );
+        PatientDisease patientDisease= new PatientDisease();
+        KieSession ksession=KieServices.Factory.get().getKieClasspathContainer().newKieSession("diseaserules");
+        ksession.insert(p);
+        ksession.insert(patientDisease);
+        ksession.fireAllRules();
+        ksession.dispose();
+        assert(patientDisease.getMaplist().get(p).contains("GERD"));
+
+    }*/
+
+
+
+
+
+
+
+}
---
+++ new updated one/drools-training-master/drools-training-master/src/test/java/io/github/Patientd.java
@@ -0,0 +1,15 @@
+package io.github;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Patientd {
+    /*Map<Integer,Disease> map=new HashMap<>();
+    private class disease{
+        Set<String> ICD;
+        Set<String> riskfactor;
+        String prefill;
+        Set<String>
+    }*/
+}
